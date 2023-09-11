aa395b0c07626aae37eb7b0e70207d3797a51037
5Jv6EhQ4U9NLNr99ucZhbB8Qj <138229310+5Jv6EhQ4U9NLNr99ucZhbB8Qj@users.noreply.github.com>
2023-09-11 16:33:21 -0400

    Button interaction Display now responds to clicked buttons. Added id to each button. Added eventListeners to buttons.

--- javascript.js
+++ javascript.js
@@ -42,3 +42,40 @@ function operate(num1, num2, operator) {
         return divide(num1, num2);
     }
 }
+
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id === 'button__1') {
+            display.textContent += '1';
+        } else if (button.id === 'button__2') {
+            display.textContent += '2';
+        } else if (button.id === 'button__3') {
+            display.textContent += '3';
+        } else if (button.id === 'button__4') {
+            display.textContent += '4';
+        } else if (button.id === 'button__5') {
+            display.textContent += '5';
+        } else if (button.id === 'button__6') {
+            display.textContent += '6';
+        } else if (button.id === 'button__7') {
+            display.textContent += '7';
+        } else if (button.id === 'button__8') {
+            display.textContent += '8';
+        } else if (button.id === 'button__9') {
+            display.textContent += '9';
+        } else if (button.id === 'button__+') {
+            display.textContent += '+';
+        } else if (button.id === 'button__-') {
+            display.textContent += '-';
+        } else if (button.id === 'button__*') {
+            display.textContent += '*';
+        } else if (button.id === 'button__/') {
+            display.textContent += '/';
+        } else if (button.id === 'button__clear') {
+            display.textContent = '';
+        }
+    });
+});
